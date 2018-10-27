df6610bf6b211353f2e6ae4cc9d8bd76f3819fb4
chris.lego <poputeacristi@gmail.com>
2018-10-27 13:17:16 +0300

    finish vertical card

--- components/VerticalCard.js
+++ components/VerticalCard.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './style.css';
 
 class VerticalCard extends React.Component {
+
   render() {
-    const { imageUrl, title, description, btnText, btnLink } = this.props;
+    const { image, title, description, btnText, btnLink } = this.props;
     return (
-      <div>
-        <div>{imageUrl}</div>
-        <div>{title}</div>
-        <div>{description}</div>
-        <a href={btnLink}>{btnText}</a>
+      <div className="card-container">
+        <div className="card-image-wrapper align-center">
+          {typeof image === 'string' ? <img src={image} /> : image()}
+        </div>
+        <div className="margin-top-large sub-header">{title}</div>
+        <p className="paragraph card-description">{description}</p>
+        <a className="btn btn-primary btn-link align-center" href={btnLink}>{btnText}</a>
       </div>
     );
   }
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, withKnobs } from '@storybook/addon-knobs';
+import './style.css';
 
 import VerticalCard from '../components/VerticalCard';
 
@@ -14,9 +15,54 @@ storiesOf('Cards', module)
     const imageUrl = text("Image Url", "");
 
     return (
-      <VerticalCard
-        title={title} description={description} imageUrl={imageUrl}
-        btnText={btnText} btnLink={btnLink}
-      />
+      <div className="storybook-container">
+        <VerticalCard
+          title={title} description={description} image={() => {
+          return <i className="fas fa-users card-image-icon" />;
+        }}
+          btnText={btnText} btnLink={btnLink}
+        />
+        <VerticalCard
+          title={title} description={description} image={imageUrl}
+          btnText={btnText} btnLink={btnLink}
+        />
+        <VerticalCard
+          title={title} description={description} image={imageUrl}
+          btnText={btnText} btnLink={btnLink}
+        />
+      </div>
+    );
+  })
+  .add('EOS Nation Section', () => {
+    return (
+      <div>
+        <h2 className="section-header text-align-center">Why vote for us?</h2>
+        <div className="storybook-container">
+          <VerticalCard
+            title={"Community"}
+            description={"We're dedicated to fostering the EOS platform through reliable and efficient block production and community engagement."}
+            image={() => {
+              return <i className="fas fa-users card-image-icon" />;
+            }}
+            btnText={"Learn more"} btnLink={"www.google.com"}
+          />
+          <VerticalCard
+            title={"Ambassadors"}
+            description={"An EOS Nation Ambassador is an individual that is passionate about EOS projects and wants to play a part in that ecosystem."}
+            image={() => {
+              return <i className="fas fa-globe-americas card-image-icon" />;
+            }}
+            btnText={"Learn more"} btnLink={"www.google.com"}
+          />
+          <VerticalCard
+            title={"Tools"}
+            description={"If EOS Nation is \"elected\" as Block Producer, two Elastic Compute Cloud (EC2) instances will be running as full EOS nodes."}
+            image={() => {
+              return <i className="fas fa-server card-image-icon" />;
+            }}
+            btnText={"Learn more"} btnLink={"www.google.com"}
+          />
+        </div>
+      </div>
     );
   });
