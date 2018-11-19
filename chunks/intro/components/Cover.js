d27794ba44bd646890ebfbc451e63beb514daac3
Carmel <andi@fluidtrends.com>
2018-11-19 08:35:47 +0200

    Add more sections to the main page

--- chunks/intro/components/Cover.js
+++ chunks/intro/components/Cover.js
@@ -6,16 +6,6 @@ import moment from 'moment'
 
 import 'antd/dist/antd.css';
 
-const socialMediaLinks = {
-  facebook: 'https://www.facebook.com/carmelio-347131802460343/',
-  medium: 'http://medium.com/carmelplatform',
-  instagram: 'https://www.instagram.com/carmel.io/',
-  github: 'https://github.com/fluidtrends/carmel',
-  youtube: 'https://www.youtube.com/channel/UCjiQXohOk0pBmJ6PFElQL-g',
-  twitter: 'https://twitter.com/carmelplatform',
-  linkedin: 'https://www.linkedin.com/company/carmel-platform/'
-}
-
 export default class Intro extends Component {
   constructor(props) {
     super(props)
@@ -33,45 +23,43 @@ export default class Intro extends Component {
   }
 
   main() {
-    const video = `assets/background.mp4`
-
     return <div style={{
       height: "100vh",
       width: "100vw",
       display: "flex",
       position: "relative",
       alignItems: "center",
-      backgroundImage: 'url(../../../assets/cover.r.png)',
-      backgroundSize: 'cover',
+      background: "rgba(0,0,0,0.7)",
+      backgroundImage: 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.1), rgba(231, 231, 231, 0.1), rgba(207, 207, 207, 0.1), rgba(184, 184, 184, 0.1), rgba(161, 161, 161, 0.1))',
       flexDirection: "column",
       justifyContent: "center"
     }}>
       <div style={{
         position: "absolute",
-        background: "rgba(0,0,0,0.7)",
-        backgroundImage: 'linear-gradient(to right bottom, rgba(255, 255, 255, 0.1), rgba(231, 231, 231, 0.1), rgba(207, 207, 207, 0.1), rgba(184, 184, 184, 0.1), rgba(161, 161, 161, 0.1))',
+        backgroundSize: 'cover',
+        backgroundImage: 'url(../../../assets/cover.r.png)',
         top: 0,
         right: 0,
         bottom: 0,
         left: 0,
-         display: "flex",
-         alignItems: "center",
-         flexDirection: "column",
-         justifyContent: "center"
-      }}>
-      <div style={{
-        width: "80vw",
-        flex: 1,
-        height: "100%",
         display: "flex",
         alignItems: "center",
         flexDirection: "column",
+        justifyContent: "center"
+      }}>
+      <div style={{
+        width: '90vw',
+        height: "100%",
+        display: "flex",
+        alignItems: "start",
+        flexDirection: "column",
         justifyContent: "center",
-        textAlign: "center"
+        textAlign: "start"
       }}>
         {this.title()}
         {this.subtitle()}
         </div>
+      {this.icons()}
       </div>
     </div>
   }
@@ -93,35 +81,17 @@ export default class Intro extends Component {
     const days = moment('17 Dec 2018').diff(moment(), 'days')
 
     return <div >
-        <Typography use='headline5' style={{
-          textAlign: 'center',
-          color: '#fafafa'
-        }}>
-        Carmel is the leading <strong> Tech Education Blockchain Platform</strong>,
-        with a powerful <strong> Working Product</strong>, a unique <strong> Educational Model </strong>
-        and a growing <strong> User Community </strong> of <strong> 50,000+ </strong>
-      </Typography>
-        <h3 key='4' style={{ color: '#602f15', marginTop: "20px", textAlign: 'center' }}>
-          <Button
-            raised
-            theme='secondary-bg text-primary-on-secondary'
-            style={{ marginTop: '30px' }}
-            onClick={this.props.onStart}>
-            <ButtonIcon icon='check_circle' />
-              Become An Early Adopter
-          </Button>
-        </h3>
-        <Typography use='headline7' style={{
-          textAlign: 'center',
-          color: '#fafafa'
-        }}>
-      </Typography>
-      </div>
+        <div>
+          <Button raised style={{background: '#D66C44', cursor: 'pointer', height: 60, width: 180, marginRight: 20}}>Learn More</Button>
+          <Button raised style={{background: '#2c4858', cursor: 'pointer', height: 60, width: 180}}>Vote</Button>
+        </div>
+    </div>
   }
 
   icons() {
-    return [<div style={{ opacity: 1.2, display: 'flex', justifyContent: 'flex-end', paddingRight: 100, paddingBottom: 30 }}>
-      <Components.SocialIcons iconColor={'#B2EBF2'} iconColorHover={'#00ACC1'} isSmallScreen={this.props.isSmallScreen} socialMediaLinks={socialMediaLinks}/>
+    const socialMediaLinks = this.props.footer.socialMediaLinks
+    return [<div style={{ display: 'flex', position: 'absolute', right: '-60px', paddingRight: 100, paddingBottom: 30 }}>
+      <Components.SocialIcons iconColor={'#ffffff'} vertical iconColorHover={'#2c4858'} size={24} isSmallScreen={this.props.isSmallScreen} socialMediaLinks={socialMediaLinks}/>
     </div>]
   }
 
--- chunks/intro/components/TwitterSection.js
+++ chunks/intro/components/TwitterSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Component } from 'react-dom-chunky'
 import TwitterCard from './TwitterCard'
+import { Heading4 } from './StyledComponents';
 
 class TwitterSection extends Component {
   constructor(props) {
@@ -17,27 +18,23 @@ class TwitterSection extends Component {
   }
 
   renderComponent() {
-    const { users, title } = this.props
+    const { users, title, textColor  } = this.props
 
-    return <div style={{display: 'flex', flexFlow: 'wrap', justifyContent: 'center'}}>
-      {users.map((user) => (
-            <TwitterCard
-              link={user.link} userURL={user.userURL} name={user.name} time={user.time} date={user.date}
-              author={user.author} kudos={user.kudos} description={user.description}
-            />))}
-      </div>
-    return (
-      <div style={{ padding: '40px 0' }}>
-        <h2 className="section-header text-align-center">Twitter Feed</h2>
-        <div className="cards-wrapper">
-          {users.map((user) => (
-            <TwitterCard
-              link={user.link} userURL={user.userURL} name={user.name} time={user.time} date={user.date}
-              author={user.author} kudos={user.kudos} description={user.description}
-            />))}
+    return <div>
+      <Heading4 center className="section-header padding-top-large margin-bottom-large" style={{color: textColor}}>
+        {title}
+      </Heading4>
+      <div style={{display: 'flex', flexFlow: 'wrap', justifyContent: 'center'}}>
+        {users.map((user) => (
+              <TwitterCard
+                link={user.link} userURL={user.userURL} name={user.name} time={user.time} date={user.date}
+                author={user.author} kudos={user.kudos} description={user.description}
+              />
+            )
+          )
+        }
         </div>
       </div>
-    );
   }
 }
 
